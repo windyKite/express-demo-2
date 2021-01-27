622b03466939ebca82de5a75968777c7c30fdded
萧泳森 <xiaoys@ligeit.com>
2021-01-27 15:12:17 +0800

    feat: 实现一个自定义中间件 logger

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const logger = require('./logger')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// 自定义中间件
+app.use(logger('dev'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use((request, response, next) => {
+  // response.send('hi') // 响应只能发送一次
+  response.write('hi') // 响应只能发送一次
+  next()
+})
+
+app.use((request, response, next) => {
+  response.write('hi')
+  next()
+})
+
+app.use((request, response, next) => {
+  response.end()
+  next()
+})
+
+app.listen(3001, () => {
+  console.log('正在 listen 3001 端口!')
+})
